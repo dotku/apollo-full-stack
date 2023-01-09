91eae58406a965ec6f53d81a452bd633ce793b13
Weijing Lin <weijingjaylin@gmail.com>
2023-01-09 03:51:53 -0800

    add placeholder and make create optional

--- src/page/frontend-shopping-list/components/ShoppingItemAddForm.tsx
+++ src/page/frontend-shopping-list/components/ShoppingItemAddForm.tsx
@@ -3,27 +3,30 @@ import { FormEvent } from "react";
 
 export default function ShoppingItemAddForm({
   value,
+  placeholder,
   handleItemCreate,
   handleInputChange,
 }: {
   value?: string;
-  handleItemCreate: (e: FormEvent<ShoppingListFormElement>) => void;
-  handleInputChange?: (e: FormEvent<HTMLInputElement>) => void;
+  placeholder?: string;
+  handleItemCreate?: (e: FormEvent<ShoppingListFormElement>) => void;
+  handleInputChange?: any;
 }) {
   return (
     <form onSubmit={handleItemCreate}>
       <div className="input-group">
         <input
-          value={value || ""}
           id="contentInput"
-          placeholder="Place Your Item"
+          placeholder={placeholder || "Place Your Item"}
           className="form-control"
           onChange={handleInputChange}
           required
         />
-        <button className="btn btn-outline-secondary" type="submit">
-          Add
-        </button>
+        {handleItemCreate && (
+          <button className="btn btn-outline-secondary" type="submit">
+            Add
+          </button>
+        )}
       </div>
     </form>
   );
--- src/page/restful-shopping-list/index.tsx
+++ src/page/restful-shopping-list/index.tsx
@@ -1,20 +1,56 @@
+import { debounce } from "lodash";
+import { ChangeEvent, useCallback, useEffect, useMemo, useState } from "react";
 import { Helmet } from "react-helmet";
+import ShoppingItemAddForm from "../frontend-shopping-list/components/ShoppingItemAddForm";
 import { RESTfulShoppingListContent } from "./RESTfulShoppingListContent";
 
+interface Env {
+  REACT_APP_DEBOUNCE_WAIT?: number;
+}
+
+const { REACT_APP_DEBOUNCE_WAIT } = process.env as Env;
 const TITLE = "RESTful Shoping List";
 
 export default function RESTfulShoppingList() {
+  const [keywords, setKeywords] = useState<string>("");
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setKeywords(e.target.value);
+  };
+
+  const debounceHandleInputChange = debounce(
+    handleInputChange,
+    REACT_APP_DEBOUNCE_WAIT
+  );
+
+  useEffect(() => {
+    return () => {
+      debounceHandleInputChange.cancel();
+    };
+  });
+
+  const RESTfulShoppingListContentCallback = useMemo(
+    () => (
+      <RESTfulShoppingListContent
+        keywords={keywords}
+        server={"http://localhost:3030/shopping-items"}
+      />
+    ),
+    [keywords]
+  );
+
   return (
     <div className="container">
       <Helmet>
         <title>{TITLE}</title>
       </Helmet>
       <h2 className="pt-3">{TITLE}</h2>
-      <div className="input-group">
-        <input className="form-control" />
-        <button className="btn btn-outline-secondary">Add</button>
-      </div>
+      <ShoppingItemAddForm
+        handleInputChange={debounceHandleInputChange}
+        placeholder="Enter your words to filter or create new"
+      />
       <RESTfulShoppingListContent
+        keywords={keywords}
         server={"http://localhost:3030/shopping-items"}
       />
     </div>
