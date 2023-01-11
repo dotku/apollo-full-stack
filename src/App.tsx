a5bedcb70186d9ebc2b81a3a00637e2916b31225
Weijing Lin <weijingjaylin@gmail.com>
2023-01-10 23:33:14 -0800

    add night mode with router modification

--- src/App.tsx
+++ src/App.tsx
@@ -6,32 +6,61 @@ import AppReact from "./App.React";
 import RESTfulShoppingList from "./pages/restful-shopping-list";
 import ZustandGetStarted from "./pages/zustand-get-started";
 import ZustandShoppingList from "./pages/zustand-shopping-list";
+import Home from "./pages/home";
 
-// @note, use hashRouter here for github hosting
-// github page won't able to support browserRouter due to server-side rendering
-// feature was disabled.
-const router = createHashRouter([
+interface FullRouter {
+  name: string;
+  description?: string;
+  path: string;
+  element: JSX.Element;
+}
+
+export const fullRouter: FullRouter[] = [
   {
+    name: "Index",
+    description: "The home page display the content of table",
     path: "/",
+    element: <Home />,
+  },
+  {
+    name: "app react",
+    description:
+      "Default React App, a backup for future potential need; eg a onboarding practice page?",
+    path: "/react-default-home",
     element: <AppReact />,
   },
   {
+    name: "frontend",
+    description: "A basic frontend interaction sample",
     path: "/frontend",
     element: <FrontendShoppingList defaultData={defaultShoppingItems} />,
   },
   {
+    name: "Restful by using custom useFetch",
+    description: "A good sample for basic frontend interaction",
     path: "/restful",
     element: <RESTfulShoppingList />,
   },
   {
+    name: "zustand sample project",
+    description: "A default sample grab from Zustand tutorial",
     path: "/zustand-get-started",
     element: <ZustandGetStarted />,
   },
   {
+    name: "zustand shopping list",
+    description: "Use Zustand created a shopping list page",
     path: "/zustand-shopping-list",
     element: <ZustandShoppingList />,
   },
-]);
+];
+
+// @note, use hashRouter here for github hosting
+// github page won't able to support browserRouter due to server-side rendering
+// feature was disabled.
+const router = createHashRouter(
+  fullRouter.map(({ path, element }) => ({ path, element }))
+);
 
 function App() {
   return <RouterProvider router={router} />;
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,68 @@
+import { useState } from "react";
+import { Helmet } from "react-helmet";
+import { Link } from "react-router-dom";
+import { fullRouter } from "../../App";
+
+const TITLE = "Home";
+
+function ViewModeSwitch({
+  onClick,
+  isNight,
+}: {
+  onClick: () => void;
+  isNight: boolean;
+}) {
+  return (
+    <div className="form-check form-switch py-3">
+      <input
+        className="form-check-input"
+        type="checkbox"
+        role="switch"
+        id="viewModeSwitch"
+        onClick={onClick}
+      ></input>
+      <label
+        className={`form-check-label ${isNight && "text-light"}`}
+        htmlFor="viewModeSwitch"
+      >
+        {isNight ? "Dark" : "Light"}
+      </label>
+    </div>
+  );
+}
+export default function Home() {
+  const [isNight, setIsNight] = useState<boolean>(true);
+  return (
+    <div
+      className="min-vh-100"
+      style={{ backgroundColor: isNight ? "rgb(11,17,35)" : "inherit" }}
+    >
+      <Helmet>
+        <title>{TITLE}</title>
+      </Helmet>
+      <div className="container">
+        <header className="d-flex justify-content-between">
+          <h1 className={`pt-3 pb-2 ${isNight && "text-light"}`}>{TITLE}</h1>
+          <ViewModeSwitch
+            onClick={() => setIsNight(!isNight)}
+            isNight={isNight}
+          />
+        </header>
+        {fullRouter.map((item, idx) => (
+          <div className="card mb-3 shadow" key={idx} style={{ maxWidth: 500 }}>
+            <div className="card-body">
+              <div className="card-title">
+                <Link to={item.path} className="text-capitalize">
+                  {item.name}
+                </Link>
+              </div>
+              {item.description && (
+                <div className="card-text">{item.description}</div>
+              )}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
